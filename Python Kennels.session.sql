9a950d6bb9c0ef1800b412e38660ee373881bc07
mgjeffries <gib.jeffries@gmail.com>
2020-10-12 13:23:06 -0500

    add methods to get customers from database

--- Python Kennels.session.sql
+++ Python Kennels.session.sql
@@ -62,12 +62,3 @@ SELECT * FROM Customer;
 SELECT * FROM Employee;
 SELECT * FROM Location;
 
-SELECT
-    a.id,
-    a.name,
-    a.breed,
-    a.status,
-    a.customer_id,
-    a.location_id
-FROM animal a
-WHERE a.id = 3
