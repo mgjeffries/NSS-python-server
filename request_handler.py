8451534911161554ce584baf66f66356908f9d78
mgjeffries <gib.jeffries@gmail.com>
2020-10-13 10:08:37 -0500

    add query method to get animals by location id

--- request_handler.py
+++ request_handler.py
@@ -1,4 +1,4 @@
-from animals import get_all_animals, get_single_animal, create_animal, delete_animal, update_animal
+from animals import get_all_animals, get_single_animal, create_animal, delete_animal, update_animal, get_animals_by_locationId
 from customers import get_all_customers, get_single_customer, create_customer, delete_customer, update_customer, get_customers_by_email
 from employees import get_all_employees, get_single_employee, create_employee, delete_employee, update_employee
 from http.server import BaseHTTPRequestHandler, HTTPServer
@@ -80,6 +80,8 @@ class HandleRequests(BaseHTTPRequestHandler):
             # email as a filtering value?
             if key == "email" and resource == "customers":
                 response = get_customers_by_email(value)
+            if key == "locationId" and resource == "animals":
+                response = get_animals_by_locationId(value)
 
         self.wfile.write(response.encode())
 
