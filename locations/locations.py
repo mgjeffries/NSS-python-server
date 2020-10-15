91bf51994e12bf9acd24bb0a5d88cebdc9a66ccb
mgjeffries <gib.jeffries@gmail.com>
2020-10-15 13:28:59 -0500

    add request handler for locations

---
+++ locations/locations.py
@@ -0,0 +1,38 @@
+import json 
+import sqlite3
+from models import Location
+
+def get_all_locations():
+    # Open a connection to the database
+    with sqlite3.connect("./kennel.db") as conn:
+
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+
+        # Write the SQL query to get the information you want
+        db_cursor.execute("""
+        SELECT
+            l.id,
+            l.name,
+            l.address
+        FROM location l
+        """)
+
+        # Initialize an empty list to hold all location representations
+        locations = []
+
+        # Convert rows of data into a Python list
+        dataset = db_cursor.fetchall()
+
+        # Iterate list of data returned from database
+        for row in dataset:
+
+            # Create an location instance from the current row.
+            # Note that the database fields are specified in
+            # exact order of the parameters defined in the
+            # location class above.
+            location = Location(row['id'], row['name'], row['address'] )
+            locations.append(location.__dict__)
+
+    # Use `json` package to properly serialize list as JSON
+    return json.dumps(locations)
