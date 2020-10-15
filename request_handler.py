9b8c94267ab326bc01ca1d03edb763ce4fdbe024
mgjeffries <gib.jeffries@gmail.com>
2020-10-15 13:35:10 -0500

    get employees if requested

--- request_handler.py
+++ request_handler.py
@@ -69,6 +69,11 @@ class HandleRequests(BaseHTTPRequestHandler):
                     response = f"{get_single_customer(id)}"
                 else:
                     response = f"{get_all_customers()}"
+            elif resource == "employees":
+                if id is not None:
+                    response = f"{get_single_employee(id)}"
+                else:
+                    response = f"{get_all_employees()}"
             elif resource == "locations":
                 response = f"{get_all_locations()}"
 
